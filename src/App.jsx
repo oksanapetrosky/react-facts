a772426eff14ba0937b24577994bf4ee3638108c
Oksana Petrosky <165575688+oksanapetrosky@users.noreply.github.com>
2025-05-18 18:21:34 +0000

    Added style for our app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import reactLogo from './assets/react-logo.png'
+import './App.css'
 
 function App() {
   return (
     <div>
-    <img src={reactLogo} alt="" />
+    <img src={reactLogo} alt="logo" />
+    <h1>This is another element</h1>
     </div>
   )
 }
